ccf3bf8d0e0abdfcefcd97e8d91c129c6c2dc988
Neil Chen <jingsi.chen@qq.com>
2019-11-29 21:36:44 +0800

    add 2.3 code
    Signed-off-by: Neil Chen <jingsi.chen@qq.com>

---
+++ unit2/apostrophe.py
@@ -0,0 +1,5 @@
+message = "One of python's strength"
+print(message)
+
+message = 'one of python's strength
+print(message)
---
+++ unit2/name.py
@@ -0,0 +1,12 @@
+name = "ada lovelace"
+print(name.title())
+
+name = "Murphy Chen"
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "lace"
+full_name = first_name + " " + last_name
+message = "hello " + full_name.title() + "!"
+print(message)
