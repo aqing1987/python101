2a2f7d9f292b3e2f3e6582681b2157925cf45bff
Neil Chen <jingsi.chen@qq.com>
2019-12-01 13:47:32 +0800

    add 5.2 code
    Signed-off-by: Neil Chen <jingsi.chen@qq.com>

---
+++ crash-course/unit5/magic_number.py
@@ -0,0 +1,16 @@
+age = 18
+print(age == 18)
+
+answer = 17
+if answer != 42:
+    print("That is not the correct answer. Please try again!")
+
+age0 = 22
+age1 = 22
+if (age0 >= 21) and (age1 >= 21):
+    print("both larger than 21")
+
+age0 = 22
+age1 = 18
+if (age0 >= 21) or (age1 >= 21):
+    print("at least one of larger than 21")
---
+++ crash-course/unit5/toppings.py
@@ -0,0 +1,11 @@
+requested_topping = 'mushrooms'
+
+if requested_topping != 'anchovies':
+    print("Hold the anchovies!")
+
+requested_toppings = ['mushrooms', 'onions', 'pineapple']
+if 'mushrooms' in requested_toppings:
+    print("mushrooms in")
+
+if 'pepperoni' in requested_toppings:
+    print('pepperoni in')
