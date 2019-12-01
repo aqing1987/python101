a13431279dc90a6ac5a85593c51e5ff01e4c2c09
Neil Chen <jingsi.chen@qq.com>
2019-12-01 19:29:35 +0800

    add 8.2 code
    Signed-off-by: Neil Chen <jingsi.chen@qq.com>

---
+++ crash-course/unit8/greeter.py
@@ -0,0 +1,12 @@
+def greet_user():
+    """show simple greeting"""
+    print("Hello!")
+
+
+def greet_user2(username):
+    """show simple greeting2"""
+    print("Hello, " + username.title() + "!")
+
+
+greet_user()
+greet_user2('jesse')
---
+++ crash-course/unit8/pets.py
@@ -0,0 +1,22 @@
+def describe_pet(animal_type, pet_name):
+    """show pets info"""
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+
+def describe_pet2(pet_name, animal_type='dog'):
+    """show pets info"""
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+
+describe_pet('hamster', 'harry')
+describe_pet('dog', 'willie')
+
+describe_pet(animal_type='hamster', pet_name='harry')
+describe_pet(pet_name='harry', animal_type='hamster')
+
+describe_pet2('Willie2')
+describe_pet2(pet_name='harry2', animal_type='hamster')
+
+# describe_pet()
