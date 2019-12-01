44e2113189328622cb8c4bd4a453a2c8626daf5e
Neil Chen <jingsi.chen@qq.com>
2019-12-01 12:14:15 +0800

    add 4.3 code
    Signed-off-by: Neil Chen <jingsi.chen@qq.com>

---
+++ crash-course/unit4/magicians.py
@@ -0,0 +1,6 @@
+magicians = ['alice', 'david', 'carlina']
+for magician in magicians:
+    print(magician)
+    print(magician.title() + ', that was a great trick!\n')
+
+print('Thank you, everyone. That was a great magic show!')
---
+++ crash-course/unit4/numbers.py
@@ -0,0 +1,13 @@
+for value in range(1, 5):
+    print(value)
+
+numbers = list(range(1, 6))
+print(numbers)
+
+even_numbers = list(range(2, 11, 2))
+print(even_numbers)
+
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print(min(digits))
+print(max(digits))
+print(sum(digits))
