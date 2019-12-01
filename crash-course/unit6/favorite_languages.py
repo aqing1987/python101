f7e4c028a49454239033055dea44e00ce1e1304d
Neil Chen <jingsi.chen@qq.com>
2019-12-01 15:38:25 +0800

    add 6.3 code
    Signed-off-by: Neil Chen <jingsi.chen@qq.com>

--- crash-course/unit6/favorite_languages.py
+++ crash-course/unit6/favorite_languages.py
@@ -9,6 +9,10 @@ print("Sarah's favorite language is " +
       favorite_languages['sarah'].title() +
       ".")
 
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorite language is " +
+          language.title())
+
 jin_pin = {
     'first_name': 'jinpin',
     'last_name': 'z',
@@ -16,3 +20,30 @@ jin_pin = {
     'city': 'hb',
 }
 print(jin_pin)
+
+
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(name.title())
+
+    if name in friends:
+        print(" Hi " + name.title() +
+              ", I see your favorite language is " +
+              favorite_languages[name].title() + "!")
+
+if 'erin' not in favorite_languages.keys():
+    print("Erin, please take our poll!")
+
+# travel the sorted output
+for name in sorted(favorite_languages.keys()):
+    print(name.title() + ", thank you for taking the poll.")
+
+# travel the dictionary values
+print("\nThe following languages have been mentioned:")
+for language in favorite_languages.values():
+    print(language.title())
+
+# use set to fix repeat values
+print("\nUse set to remove repeated values")
+for language in set(favorite_languages.values()):
+    print(language.title())
---
+++ crash-course/unit6/user.py
@@ -0,0 +1,9 @@
+user0 = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi',
+}
+
+for key, value in user0.items():
+    print("\nKey: " + key)
+    print("Value: " + value)
