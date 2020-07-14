bfcb79d5ee988f1bb9525573cc3eded3032aa47e
Neil Chen <jingsi.chen@qq.com>
2020-07-14 21:44:18 +0800

    alien invasion: show ships left
    Signed-off-by: Neil Chen <jingsi.chen@qq.com>

--- crash-course/alien_invasion/game_functions.py
+++ crash-course/alien_invasion/game_functions.py
@@ -68,6 +68,7 @@ def check_play_button(ai_settings, screen, stats, sb, play_button, ship,
         sb.prep_score()
         sb.prep_high_score()
         sb.prep_level()
+        sb.prep_ships()
 
         aliens.empty()
         bullets.empty()
@@ -193,22 +194,25 @@ def check_fleet_edges(ai_settings, aliens):
             break
 
 
-def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
+def update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets):
     """update all aliens position"""
     check_fleet_edges(ai_settings, aliens)
     aliens.update()
 
     # check if alien hit ship
     if pygame.sprite.spritecollideany(ship, aliens):
-        ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+        ship_hit(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
-    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
+    check_aliens_bottom(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
 
-def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
+def ship_hit(ai_settings, screen, stats, sb, ship, aliens, bullets):
     """respond to alien hit ship"""
     if stats.ships_left > 0:
         stats.ships_left -= 1
+
+        # update score board
+        sb.prep_ships()
 
         # clear alien and bullets
         aliens.empty()
@@ -226,13 +230,13 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
         pygame.mouse.set_visible(True)
 
 
-def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+def check_aliens_bottom(ai_settings, screen, stats, sb, ship, aliens, bullets):
     """check if alien reached screen bottom"""
     screen_rect = screen.get_rect()
     for alien in aliens.sprites():
         if alien.rect.bottom >= screen_rect.bottom:
             # process like alien hit ship
-            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            ship_hit(ai_settings, screen, stats, sb, ship, aliens, bullets)
             break
 
 def check_high_score(stats, sb):
