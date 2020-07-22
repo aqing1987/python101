1f431ced493d73d0be0b255239e8263795977da2
Neil Chen <jingsi.chen@qq.com>
2020-07-22 22:29:05 +0800

    crash course: random walk: hide axis

--- crash-course/data-visualization/random_walk/rw_visual.py
+++ crash-course/data-visualization/random_walk/rw_visual.py
@@ -20,6 +20,11 @@ while True:
     # highlight start and end points
     plt.scatter(0, 0, c='green', edgecolor='none', s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolor='none', s=100)
+
+    # hide axis
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
     plt.show()
 
     keep_running = input("Make another walk ? (y/n): ")
