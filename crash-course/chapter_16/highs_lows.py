b16d20244bd1dffddbc48e6fed056947ba3139f7
Neil Chen <jingsi.chen@qq.com>
2020-07-28 07:22:34 +0800

    crash course: c16: draw temperature figure

--- crash-course/chapter_16/highs_lows.py
+++ crash-course/chapter_16/highs_lows.py
@@ -1,5 +1,7 @@
 import csv
 
+from matplotlib import pyplot as plt
+
 filename = 'sitka_weather_07-2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
@@ -16,3 +18,15 @@ with open(filename) as f:
         high = int(row[1])
         highs.append(high)
     print(highs)
+
+# draw according to data
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(highs, c='red')
+
+# set figure format
+plt.title("Daily high temperature, July 2014", fontsize=24)
+plt.xlabel("", fontsize=16)
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
