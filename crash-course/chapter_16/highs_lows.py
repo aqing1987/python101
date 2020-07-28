dbc24c7bbd6e29582f7c60c909ab4a3a9ee3977f
Neil Chen <jingsi.chen@qq.com>
2020-07-28 08:43:51 +0800

    crash course: c16: use full year data

--- crash-course/chapter_16/highs_lows.py
+++ crash-course/chapter_16/highs_lows.py
@@ -3,7 +3,8 @@ from datetime import datetime
 
 from matplotlib import pyplot as plt
 
-filename = 'sitka_weather_07-2014.csv'
+#filename = 'sitka_weather_07-2014.csv'
+filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -29,7 +30,7 @@ fig = plt.figure(dpi=128, figsize=(10, 6))
 plt.plot(dates, highs, c='red')
 
 # set figure format
-plt.title("Daily high temperature, July 2014", fontsize=24)
+plt.title("Daily high temperature, 2014", fontsize=24)
 plt.xlabel("", fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
