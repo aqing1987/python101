0e4be0c6d18860232bed283e2dc269553c62512e
Neil Chen <jingsi.chen@qq.com>
2020-07-28 22:15:52 +0800

    crash course: c16: show figure about death valley

--- crash-course/chapter_16/highs_lows.py
+++ crash-course/chapter_16/highs_lows.py
@@ -4,7 +4,8 @@ from datetime import datetime
 from matplotlib import pyplot as plt
 
 #filename = 'sitka_weather_07-2014.csv'
-filename = 'sitka_weather_2014.csv'
+#filename = 'sitka_weather_2014.csv'
+filename = 'death_valley_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -17,17 +18,17 @@ with open(filename) as f:
     # extract datetime, max temperature and min temperature
     dates, highs, lows = [], [], []
     for row in reader:
-        # 0 datetime
-        current_date = datetime.strptime(row[0], "%Y-%m-%d")
-        dates.append(current_date)
-        # 1 Max Temperature
-        high = int(row[1])
-        highs.append(high)
-
-        # 3 Min Temperature
-        low = int(row[3])
-        lows.append(low)
-    #print(highs)
+        # 0 datetime, 1 max temperature, 3 min temperature
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 # draw according to data
 fig = plt.figure(dpi=128, figsize=(10, 6))
@@ -36,7 +37,7 @@ plt.plot(dates, lows, c='blue', alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # set figure format
-plt.title("Daily high and low temperature, 2014", fontsize=24)
+plt.title("Daily high and low temperature, 2014\nDeath Valley, CA", fontsize=24)
 plt.xlabel("", fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
