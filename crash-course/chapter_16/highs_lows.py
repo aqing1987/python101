0a50100268eb1bfcf97ded501dcd6cb253ee1f84
Neil Chen <jingsi.chen@qq.com>
2020-07-28 21:56:31 +0800

    crash course: c16: draw min temperature

--- crash-course/chapter_16/highs_lows.py
+++ crash-course/chapter_16/highs_lows.py
@@ -14,23 +14,28 @@ with open(filename) as f:
     for index, column_header in enumerate(header_row):
         print(index, column_header)
     """
-    # extract datetime and max temperature
-    dates, highs = [], []
+    # extract datetime, max temperature and min temperature
+    dates, highs, lows = [], [], []
     for row in reader:
         # 0 datetime
         current_date = datetime.strptime(row[0], "%Y-%m-%d")
         dates.append(current_date)
-        # 1 Max TemperatureF
+        # 1 Max Temperature
         high = int(row[1])
         highs.append(high)
+
+        # 3 Min Temperature
+        low = int(row[3])
+        lows.append(low)
     #print(highs)
 
 # draw according to data
 fig = plt.figure(dpi=128, figsize=(10, 6))
 plt.plot(dates, highs, c='red')
+plt.plot(dates, lows, c='blue')
 
 # set figure format
-plt.title("Daily high temperature, 2014", fontsize=24)
+plt.title("Daily high and low temperature, 2014", fontsize=24)
 plt.xlabel("", fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
